f12c48bdd8659654d2bf9a8de4b67d0d0c49e8e7
Adil Shamuratov <iswearican_1@outlook.com>
2024-12-05 10:59:44 +0300

    clear

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,7 @@ import bs4
 # import nodriver as uc
 import undetected_chromedriver as uc
 from nodriver.core.config import Config
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -118,8 +119,15 @@ class ParsingUtils(BaseUtils):
             return text_content, ogrn, works_with_ozon
 
     async def extract_info_from_seller_page(self, products: list[dict]) -> list[dict]:
-        browser = uc.Chrome()
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--disable-blink-features=AutomationControlled")
 
+        browser = uc.Chrome(options=options)
         for product in products[:]:
             try:
                 url = product.get('seller_link')
